51a481c3bacdfddd0ff6621d4b3d449e3b0f9c05
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-25 20:55:24 +0700

    Update

--- Games/Universal.lua
+++ Games/Universal.lua
@@ -1,5 +1,5 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Ui.lua"))()
-local ChitogeX = Library.new("Chitoge X [Demo]")
+local XLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Ui.lua"))()
+local ChitogeX = XLibrary.new("Chitoge X")
 
 WalkSpeedSettings = {
     NSpeed = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed
@@ -38,43 +38,3 @@ JumpPowerSection:addToggle("Enable JumpPower", false, function(Value)
         game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = JumpPowerSettings.NJump
     end
 end)
-
-local MiscPage = ChitogeX:addPage("Misc", 3926305904)
-
-local NoClipSection = MiscPage:addSection("No Clip")
-NoClipSection:addToggle("No Clip", false, function(Value)
-local Noclipping = nil
-Clip = false
-if Value == true then
-local function NoclipLoop()
-	if Clip == false and game:GetService("Players").LocalPlayer.Character ~= nil then
-		for _, child in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
-			if child:IsA("BasePart") and child.CanCollide == true and child.Name ~= floatName then
-				child.CanCollide = false
-			end
-		end
-	end
- end
-Noclipping = game:GetService('RunService').Stepped:Connect(NoclipLoop)
-else
-    if Noclipping then
-		Noclipping:Disconnect()
-	end
-        Clip = true
-    end
-end)
-
-local SettingsPage = ChitogeX:addPage("Settings", 4483345737)
-
-local SettingsSection = SettingsPage:addSection("Settings")
-SettingsSection:addKeybind("Ui Toggle", Enum.KeyCode.RightControl, function()
-    ChitogeX:toggle()
-end)
-
-SettingsSection:addButton("Destroy Ui", function()
-    NebuleX:Notify("Nebule X", "Thank You, Good Bye", function()end)
-    wait(3)
-    game:GetService("CoreGui")["Nebule X [Demo]"]:Destroy()
-end)
-
-ChitogeX:SelectPage(ChitogeX.pages[2], true)
