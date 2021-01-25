125953a0ab67f428db3a149d2bc7e6fc043ffa32
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-25 20:47:10 +0700

    Update

--- Whitelists/ZegR9tXUjUwPYzD.lua
+++ Whitelists/ZegR9tXUjUwPYzD.lua
@@ -1,5 +1,6 @@
 if _G.Key == "ZegR9tXUjUwPYzD" then
     if game:GetService("Players").LocalPlayer.Name == "ChitogeKazuto" then
+        wait(3)
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/LoadStrings.lua",true))()
     end
 end
