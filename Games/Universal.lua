0bcb2e39a2f184e289cb4bf58d13cdb43a252c4d
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-25 20:08:41 +0700

    Update

--- Games/Universal.lua
+++ Games/Universal.lua
@@ -1,16 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Ui.lua"))()
 local ChitogeX = Library.new("Chitoge X [Demo]")
 
-
-local LocalPlayerPage = ChitogeX:addPage("LocalPlayer", 4384401919)
-local WalkSpeedSection = LocalPlayerPage:addSection("WalkSpeed")
-local JumpPowerSection = LocalPlayerPage:addSection("JumpPower")
-local NoClipSection = LocalPlayerPage:addSection("No Clip")
-
-local SettingsPage = ChitogeX:addPage("Settings", 4483345737)
-local SettingsSection = SettingsPage:addSection("Settings")
-
-
 WalkSpeedSettings = {
     NSpeed = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed
 }
@@ -19,8 +9,10 @@ JumpPowerSettings = {
     NJump = game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower
 }
 
-wait(0.1)
-WalkSpeedSection:addSlider("Value", WalkSpeedSettings.NSpeed, 0, 300, function(Value) -- WalkSpeed
+local LocalPlayerPage = ChitogeX:addPage("LocalPlayer", 4384401919)
+local WalkSpeedSection = LocalPlayerPage:addSection("WalkSpeed")
+
+WalkSpeedSection:addSlider("Speed", WalkSpeedSettings.NSpeed, 0, 300, function(Value)
     _G.Speed = Value
 end)
 WalkSpeedSection:addToggle("Enable WalkSpeed", false, function(Value)
@@ -32,7 +24,9 @@ WalkSpeedSection:addToggle("Enable WalkSpeed", false, function(Value)
         game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = WalkSpeedSettings.NSpeed
     end
 end)
-JumpPowerSection:addSlider("Value", JumpPowerSettings.NJump, 0, 300, function(Value) -- JumpPower
+
+local JumpPowerSection = LocalPlayerPage:addSection("JumpPower")
+JumpPowerSection:addSlider("Jump", JumpPowerSettings.NJump, 0, 300, function(Value)
     _G.Jump = Value
 end)
 JumpPowerSection:addToggle("Enable JumpPower", false, function(Value)
@@ -44,27 +38,42 @@ JumpPowerSection:addToggle("Enable JumpPower", false, function(Value)
         game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = JumpPowerSettings.NJump
     end
 end)
-NoClipSection:addToggle("No Clip", false, function(Value) -- NoClip
+
+local NoClipSection = LocalPlayerPage:addSection("No Clip")
+NoClipSection:addToggle("No Clip", false, function(Value)
 local Noclipping = nil
 Clip = false
-    
+
 if Value == true then
-    local function NoclipLoop()
-            if Clip == false and game:GetService("Players").LocalPlayer.Character ~= nil then
-                for _, child in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
-                    if child:IsA("BasePart") and child.CanCollide == true and child.Name ~= floatName then
-                        child.CanCollide = false
-                    end
-                end
-            end
-        end
-        Noclipping = game:GetService('RunService').Stepped:Connect(NoclipLoop)
-    else
-        if Noclipping then
-            Noclipping:Disconnect()
-        end
-        Clip = true
+local function NoclipLoop()
+		if Clip == false and game:GetService("Players").LocalPlayer.Character ~= nil then
+			for _, child in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
+				if child:IsA("BasePart") and child.CanCollide == true and child.Name ~= floatName then
+					child.CanCollide = false
+				end
+			end
+		end
     end
+Noclipping = game:GetService('RunService').Stepped:Connect(NoclipLoop)
+else
+    if Noclipping then
+		Noclipping:Disconnect()
+	end
+    Clip = true
+end
 end)
 
-ChitogeX:SelectPage(NebuleX.pages[2], true)
+local SettingsPage = ChitogeX:addPage("Settings", 4483345737)
+local SettingsSection = SettingsPage:addSection("Settings")
+
+SettingsSection:addKeybind("Ui Toggle", Enum.KeyCode.RightControl, function()
+    NebuleX:toggle()
+end)
+
+SettingsSection:addButton("Destroy Ui", function()
+    NebuleX:Notify("Nebule X", "Thank You, Good Bye", function()end)
+    wait(3)
+    game:GetService("CoreGui")["Nebule X [Demo]"]:Destroy()
+end)
+
+ChitogeX:SelectPage(ChitogeX.pages[2], true)
