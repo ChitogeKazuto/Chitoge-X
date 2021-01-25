9d838dffa27c3bb3c3c9f25197c2e9870643ab70
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-25 21:45:36 +0700

    Update

--- Games/Universal.lua
+++ Games/Universal.lua
@@ -1,6 +1,11 @@
 local XLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Ui.lua"))()
 local ChitogeX = XLibrary.new("Chitoge X")
 
+local ALLPLAYER = {}
+for i,v in pairs(game:GetService("Players"):GetChildren()) do
+    table.insert(ALLPLAYER, v.Name)
+end
+
 WalkSpeedSettings = {
     NSpeed = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed
 }
@@ -37,4 +42,59 @@ JumpPowerSection:addToggle("Enable JumpPower", false, function(Value)
     if _G.JumpPower == false then
         game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = JumpPowerSettings.NJump
     end
+end)
+
+local MiscPage = ChitogeX:addPage("Misc", 3944704135)
+
+local JumpSection = MiscPage:addSection("Jump")
+
+local flyjump
+JumpSection:addToggle("Fly Jump", false, function(Value)
+    if Value == true then
+	if flyjump then flyjump:Disconnect() end
+	flyjump = game:GetService("UserInputService").JumpRequest:Connect(function(Jump)
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+    end)
+else
+    if flyjump then flyjump:Disconnect() end
+end
+end)
+
+local NoClipSection = MiscPage:addSection("No Clip")
+
+NoClipSection:addToggle("No Clip", false, function(Value)
+noclip = true 
+    game:GetService('RunService').Stepped:connect(function()
+    if noclip then
+    game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+        end
+        end)
+    if Value == true then
+        noclip = true
+    else
+        noclip = false 
+    end
+end)
+
+NoClipSection:addToggle("Advance No Clip", false, function(Value)
+local Noclipping = nil
+Clip = false
+
+if Value == true then
+local function NoclipLoop()
+		if Clip == false and game:GetService("Players").LocalPlayer.Character ~= nil then
+			for _, child in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
+				if child:IsA("BasePart") and child.CanCollide == true and child.Name ~= floatName then
+					child.CanCollide = false
+				end
+			end
+		end
+    end
+Noclipping = game:GetService('RunService').Stepped:Connect(NoclipLoop)
+else
+    if Noclipping then
+		Noclipping:Disconnect()
+	end
+    Clip = true
+end
 end)
