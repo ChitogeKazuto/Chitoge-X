0c978688a509ce623642581f1bd0cc558eec6f89
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-26 23:00:16 +0700

    Update

--- Whitelists/ZegR9tXUjUwPYzD.lua
+++ Whitelists/ZegR9tXUjUwPYzD.lua
@@ -1,6 +1,6 @@
 if _G.Key == "ZegR9tXUjUwPYzD" then
     if game:GetService("Players").LocalPlayer.Name == "ChitogeKazuto" then
-        if _G.Ui = 1 the
+        if _G.Ui == 1 then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui1_Universal.lua",true))()
         else
             loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui2_Universal.lua",true))()
