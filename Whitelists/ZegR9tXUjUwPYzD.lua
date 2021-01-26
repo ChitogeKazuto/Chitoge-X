5f1aa73f86104a572e9b354f47695947a5cf5263
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-26 23:18:26 +0700

    Update

--- Whitelists/ZegR9tXUjUwPYzD.lua
+++ Whitelists/ZegR9tXUjUwPYzD.lua
@@ -6,7 +6,7 @@ if _G.Key == "ZegR9tXUjUwPYzD" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui2_Universal.lua",true))()
         end
     else
-        game:GetService("Players").LocalPlayer:Kick("Invalid Whitelist")
+        game:GetService("Players").LocalPlayer:Kick("Invalid Username")
     end
 else
     game:GetService("Players").LocalPlayer:Kick("Invalid Whitelist")
