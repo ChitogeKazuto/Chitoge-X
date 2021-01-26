23e6f10876fc19c3179e1c7cf5549056a31a7c55
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-26 08:46:03 +0700

    Update Test.lua

--- Games/Test.lua
+++ Games/Test.lua
@@ -3,7 +3,8 @@ local ChitogeX = Library:CreateWindow("Main")
 
 local LocalPlayerFolder = ChitogeX:AddFolder("LocalPlayer")
 
-LocalPlayerFolder:AddSlider({text=' Value', min = 0,max = 300, callback = function(Value)
+LocalPlayerFolder:AddLabel({text = "             WalkSpeed"})
+LocalPlayerFolder:AddSlider({text=' Value', value = 16, min = 0,max = 300, callback = function(Value)
     _G.Speed = Value
 end})
 LocalPlayerFolder:AddToggle({text = ' WalkSpeed', callback = function(Value)
@@ -12,6 +13,18 @@ LocalPlayerFolder:AddToggle({text = ' WalkSpeed', callback = function(Value)
     end
     if Value == false then
         game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = 16
+    end
+end})
+LocalPlayerFolder:AddLabel({text = "             JumpPower"})
+LocalPlayerFolder:AddSlider({text=' Value', value = 50, min = 0, max = 300, callback = function(Value)
+    _G.Jump = Value
+end})
+LocalPlayerFolder:AddToggle({text = ' JumpPower', callback = function(Value)
+    while Value do wait()
+        game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = _G.Jump
+    end
+    if Value == false then
+        game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = 50
     end
 end})
 
