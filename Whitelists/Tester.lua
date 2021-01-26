c3a4c312a0d607842d69432a41859dbd852e086b
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-26 23:14:35 +0700

    Update

--- Whitelists/Tester.lua
+++ Whitelists/Tester.lua
@@ -1,5 +1,5 @@
-if _G.Key == "Tester" then
-    if _G.Ui == 1 then
+if getgenv().Key == "Tester" then
+    if getgenv().Ui == 1 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui1_Universal.lua",true))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui2_Universal.lua",true))()
