f37c1533ba07a119f9e3cc6cd07a43b1902e21af
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-27 19:29:05 +0700

    Update

--- Whitelists/hPxNj6cAJWahK8c.lua
+++ Whitelists/hPxNj6cAJWahK8c.lua
@@ -1,8 +1,5 @@
-local Username = game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Whitelists/Username/Username_hPxNj6cAJWahK8c.lua",true)
-local Playername = game:GetService("Players").LocalPlayer.Name
-
 if _G.Key == "hPxNj6cAJWahK8c" then
-    if string.find(Username,Playername) then
+    if game:GetService("Players").LocalPlayer.Name == "Undertale_ver" then
         if game.PlaceId == 537413528 then
             if _G.Ui == 1 then
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Build%20A%20Boat/Ui1.Build%20A%20Boat.lua",true))()
