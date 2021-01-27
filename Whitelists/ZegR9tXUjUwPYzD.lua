5bf2e0e30bf07dc752c627a111c5c29e36e2daa3
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-27 10:17:25 +0700

    Update

--- Whitelists/ZegR9tXUjUwPYzD.lua
+++ Whitelists/ZegR9tXUjUwPYzD.lua
@@ -1,9 +1,17 @@
 if _G.Key == "ZegR9tXUjUwPYzD" then
     if game:GetService("Players").LocalPlayer.Name == "ChitogeKazuto" then
-        if _G.Ui == 1 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Universal/Ui1_Universal.lua",true))()
+        if game.PlaceId == 537413528 then
+            if _G.Ui == 1 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Universal/Ui1_Universal.lua",true))()
+            else
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Universal/Ui2_Universal.lua",true))()
+            end
         else
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Universal/Ui2_Universal.lua",true))()
+            if _G.Ui == 1 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Universal/Ui1_Universal.lua",true))()
+            else
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Universal/Ui2_Universal.lua",true))()
+            end
         end
     else
         game:GetService("Players").LocalPlayer:Kick("Invalid Username")
