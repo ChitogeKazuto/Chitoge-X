aca320bf171629c016256e38411c69f677fa1257
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-27 08:41:36 +0700

    Update

--- Whitelists/Tester.lua
+++
@@ -1,9 +0,0 @@
-if _G.Key == "Tester" then
-    if _G.Ui == 1 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui1_Universal.lua",true))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui2_Universal.lua",true))()
-     end
-else
-    game:GetService("Players").LocalPlayer:Kick("Invalid Whitelist")
-end
--- Whitelists/ZegR9tXUjUwPYzD.lua
+++ Whitelists/ZegR9tXUjUwPYzD.lua
@@ -1,9 +1,9 @@
 if _G.Key == "ZegR9tXUjUwPYzD" then
     if game:GetService("Players").LocalPlayer.Name == "ChitogeKazuto" then
         if _G.Ui == 1 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui1_Universal.lua",true))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Universal/Ui1_Universal.lua",true))()
         else
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Games/Universal/Ui2_Universal.lua",true))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Universal/Ui2_Universal.lua",true))()
         end
     else
         game:GetService("Players").LocalPlayer:Kick("Invalid Username")
