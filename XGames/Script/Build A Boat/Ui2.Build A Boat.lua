86ac8c7f983650f6bc4d46d4671a5140c19f243a
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-27 14:44:39 +0700

    Update

--- XGames/Script/Build A Boat/Ui2.Build A Boat.lua
+++ XGames/Script/Build A Boat/Ui2.Build A Boat.lua
@@ -1,5 +1,12 @@
 local Library = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Ui2.lua'))()
 
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
 WalkSpeedSettings = {
     NSpeed = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed
 }
--- XGames/Script/Universal/Ui1_Universal.lua
+++ XGames/Script/Universal/Ui1_Universal.lua
@@ -1,6 +1,13 @@
 local XLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/Ui1.lua"))()
 local ChitogeX = XLibrary.new("Chitoge X")
 
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
 WalkSpeedSettings = {
     NSpeed = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed
 }
