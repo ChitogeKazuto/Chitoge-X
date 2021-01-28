8eecb719b859f936a78f74568f087292d9ccff07
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-28 15:02:29 +0700

    Update

--- Whitelists/pOJ2eNGr52jlpKT.lua
+++ Whitelists/pOJ2eNGr52jlpKT.lua
@@ -1,16 +1,20 @@
-if _G.Key == "__pOJ2eNGr52jlpKT__" then
-    if game.PlaceId == 537413528 then
-        if _G.Ui == 1 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Build%20A%20Boat/Ui1.Build%20A%20Boat.lua",true))()
+if _G.Key == "pOJ2eNGr52jlpKT" then
+    if game:GetService("Players").LocalPlayer.Name == "Reaper_PirateTH" then
+        if game.PlaceId == 537413528 then
+            if _G.Ui == 1 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Build%20A%20Boat/Ui1.Build%20A%20Boat.lua",true))()
+            else
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Build%20A%20Boat/Ui2.Build%20A%20Boat.lua",true))()
+            end
         else
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Build%20A%20Boat/Ui2.Build%20A%20Boat.lua",true))()
+            if _G.Ui == 1 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Universal/Ui1_Universal.lua",true))()
+            else
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Universal/Ui2_Universal.lua",true))()
+            end
         end
     else
-        if _G.Ui == 1 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Universal/Ui1_Universal.lua",true))()
-        else
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Chitoge-X/master/XGames/Script/Universal/Ui2_Universal.lua",true))()
-        end
+        game:GetService("Players").LocalPlayer:Kick("Invalid Username")
     end
 else
     game:GetService("Players").LocalPlayer:Kick("Invalid Whitelist")
